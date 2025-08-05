64b120d742e0c8d631d8d956a8c290480cce936e
Parth Salunke <144267660+sparth292@users.noreply.github.com>
2025-08-05 10:59:49 +0530

    Update caesar-cipher.cpp

--- caesar-cipher.cpp
+++ caesar-cipher.cpp
@@ -10,7 +10,7 @@ int main() {
     for(int i = 0 ; i < input.length() ; i++){
         
         addChar = input[i];
-        if((addChar+3) > 122) std::cout << (char)(((addChar+2)-122) + 97);
+        if((addChar+3) > 122) std::cout << (char)(addChar + 2 - 122 + 97);
         else{
         addChar += 3;
         printChar = addChar;
